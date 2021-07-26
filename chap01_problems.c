b37ff6f1e4421c3f42c6b2df2bf8d3eef64281aa
SyedTaimoorNawaz <bantutaimoor@gmail.com>
2021-07-27 04:28:37 +0500

    all problems in uncommetted

--- chap01_problems.c
+++ chap01_problems.c
@@ -1,51 +1,51 @@
 //! write c program to calculate area of rectangle: using hard coded inputs, using input supplied by a user?
 
-/*#include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
     // using hard coded inputs (rectangle_height, rectangle_width)
-    //* int rectangleHeight = 12, rectangleWidth = 20, areaOfRectangle;
-    // area of rectangle formula 
-    // areaOfRectangle = rectangleWidth * rectangleHeight
+     int rectangleHeight = 12, rectangleWidth = 20, areaOfRectangle;
+    //  area of rectangle formula 
+     areaOfRectangle = rectangleWidth * rectangleHeight;
 
-    //* areaOfRectangle = rectangleHeight * rectangleWidth;
+     areaOfRectangle = rectangleHeight * rectangleWidth;
 
-    //* printf("\nArea of rectangle is: %d", areaOfRectangle);
+     printf("\nArea of rectangle is: %d", areaOfRectangle);
 
     // using input supplied by a user
-    //* float rectangleHeight, rectangleWidth, areaOfRectangle;
+     float rectangleHeight, rectangleWidth, areaOfRectangle;
 
-    //* printf("\nEnter a rectangle height: ");
-    //* scanf("%f", &rectangleHeight);
+     printf("\nEnter a rectangle height: ");
+     scanf("%f", &rectangleHeight);
 
-    //* printf("\nEnter rectangle widith: ");
-    //* scanf("%f", &rectangleWidth);
+     printf("\nEnter rectangle widith: ");
+     scanf("%f", &rectangleWidth);
 
         // area of rectangle formula 
-    // areaOfRectangle = rectangleWidth * rectangleHeight
-    // * areaOfRectangle = rectangleHeight * rectangleWidth;
-    // * printf("\nArea of rectangle is: %f", areaOfRectangle);
+     areaOfRectangle = rectangleWidth * rectangleHeight;
+     areaOfRectangle = rectangleHeight * rectangleWidth;
+     printf("\nArea of rectangle is: %f", areaOfRectangle);
     return 0;
-}*/
+}
 
 //! Calculate the area of circle and modify the same program to calculate the volume of the cylinder given its radius and height?
 
-/*#include <stdio.h>
+#include <stdio.h>
 // using library to solve maths function
 #include <math.h>
 int main()
 {
     // Area of circle 
-    //* float areaOfCircle, radius;
+     float areaOfCircle, radius;
     // formula of area of circle 
     // area = π · r2
 
-    //* printf("\nEnter a radius value: ");
-    //* scanf("%f", &radius);
+     printf("\nEnter a radius value: ");
+     scanf("%f", &radius);
 
-    //* areaOfCircle = M_PI * pow(radius, 2);
-    //* printf("%f", areaOfCircle);
+     areaOfCircle = M_PI * pow(radius, 2);
+     printf("%f", areaOfCircle);
 
     // Volume of cylinder 
     float volumeOfCylinder, height, radius;
@@ -53,58 +53,58 @@ int main()
     // formula of volume of cylinder 
     // volumeOfCylinder = π · r2 · h
 
-    //* printf("\nEnter a radius value: ");
-    //* scanf("%f", &radius);
+     printf("\nEnter a radius value: ");
+     scanf("%f", &radius);
 
-    //* printf("\nEnter a height value: ");
-    //* scanf("%f", &height);
+     printf("\nEnter a height value: ");
+     scanf("%f", &height);
 
-    //*  volumeOfCylinder = M_PI * pow(radius, 2) * height;
-    //* printf("\nVolume of Cylinder is: %f", volumeOfCylinder);
-    // return 0;
+      volumeOfCylinder = M_PI * pow(radius, 2) * height;
+      printf("\nVolume of Cylinder is: %f", volumeOfCylinder);
+     return 0;
 }
-*/
 
-//! write a program to convert celcius (centigrade degree temperature to fahrenheit)
 
-/*#include <stdio.h>
+//! write a program to convert celcius (centigrade degree temperature to fahrenheit)?
+
+#include <stdio.h>
 
 int main()
 {
-    //* float centigradeToFahrenehit, celcius;
-    //* printf("Enter a celcius value: ");
-    //* scanf("%f",  &celcius);
+     float centigradeToFahrenehit, celcius;
+     printf("Enter a celcius value: ");
+     scanf("%f",  &celcius);
 
     // Formula of centigradeToFahrenehit
     //  T(°F) = T(°C) × 9/5 + 32
 
-    //* centigradeToFahrenehit = (celcius * 9/5) + 32;
-    //* printf("Centigrade to fahrenehit is : %f", centigradeToFahrenehit );
+     centigradeToFahrenehit = (celcius * 9/5) + 32;
+     printf("Centigrade to fahrenehit is : %f", centigradeToFahrenehit );
 
     return 0;
-}*/
+}
 
-//! write a program to calculate simple interest for a set of values representing principal amount, rate of interest and time periods.
+//! write a program to calculate simple interest for a set of values representing principal amount, rate of interest and time periods?
 
-/*#include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    //* int principalAmount, rateOfInterest, timePeriods, simpleInterest;
+     int principalAmount, rateOfInterest, timePeriods, simpleInterest;
 
-    //* printf("\nEnter a principal amount: ");
-    //* scanf("%d", &principalAmount);
+     printf("\nEnter a principal amount: ");
+     scanf("%d", &principalAmount);
 
-    //* printf("\nEnter a rate of interest: ");
-    //* scanf("%d", &rateOfInterest);
+     printf("\nEnter a rate of interest: ");
+     scanf("%d", &rateOfInterest);
 
-    //* printf("\nEnter a time period: ");
-    //* scanf("%d", &timePeriods);
+     printf("\nEnter a time period: ");
+     scanf("%d", &timePeriods);
 
     // Formula of simpleInterest
     // SI = (P × R ×T) / 100
 
-    //* simpleInterest = (principalAmount * rateOfInterest * timePeriods) / 100;
-    //* printf("Simple Interest is :%d", simpleInterest);
+     simpleInterest = (principalAmount * rateOfInterest * timePeriods) / 100;
+     printf("Simple Interest is :%d", simpleInterest);
     return 0;
-}*/
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,22 @@
+/*#include <stdio.h>
+
+int main()
+{
+    //* int principalAmount, rateOfInterest, timePeriods, simpleInterest;
+
+    //* printf("\nEnter a principal amount: ");
+    //* scanf("%d", &principalAmount);
+
+    //* printf("\nEnter a rate of interest: ");
+    //* scanf("%d", &rateOfInterest);
+
+    //* printf("\nEnter a time period: ");
+    //* scanf("%d", &timePeriods);
+
+    // Formula of simpleInterest
+    // SI = (P × R ×T) / 100
+
+    //* simpleInterest = (principalAmount * rateOfInterest * timePeriods) / 100;
+    //* printf("Simple Interest is :%d", simpleInterest);
+    return 0;
+}*/
