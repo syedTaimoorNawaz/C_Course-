f4022fddfb458ee09172cbe50cdf701ac4953481
Syed Taimoor Nawaz <bantutaimoor@gmail.com>
2021-07-27 04:06:57 +0500

    add initilize

---
+++ chap01_Variables_Constants_Keywords.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 12;     // it is interger constant and a is a varianle
+    float f = 12.3; // it is real constant and b is a variable
+    char c = '$';   // it is a charecter constant it must use single qout and c is a variable
+    int e, r;// e is decleared
+    printf("hello taimoor");
+    printf("\nthis is a integer %d", i);    
+    printf("\nthis is a float %f", f);    
+    printf("\nthis is a char %c", c);    
+
+    printf("\nEnter any number:");
+    scanf("\n%d", &r);
+    printf("%d", r);
+    return 0;
+    
+}
